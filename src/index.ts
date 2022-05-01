4a7eac976aff82b6c8285db37bbf08d9ec00b685
SIMONETTIGUIDO1993 <104177337+SIMONETTIGUIDO1993@users.noreply.github.com>
2022-05-01 01:35:03 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+let precioInicial: number = 450.5;
+console.log("el precio del producto es $", precioInicial);
+
+let descuento: number = 0.1;
+console.log("descuento aplicado ", descuento);
+
+let precioConDescuento: number = precioInicial * descuento;
+
+let precioFinal: number = precioInicial - precioConDescuento;
+console.log("el precio con descuento es $", precioFinal);
